ef9ba090ab66753380618e191a7ea5b60dbf5af8
xdarkphoneix <rajsaurav589@gmail.com>
2024-05-15 14:31:16 +0530

    adding api errror and api response file and aysync handler

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,17 @@
+class ApiResponse {
+constructor(
+    statuscode,
+    message="Sucess",
+    data
+
+){
+    this.statuscode=statuscode,
+    this.data=data,
+    this.message=message,
+    this.sucess=statuscode<400
+
+}
+
+
+
+}
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,11 @@
+import { reset } from "nodemon"
+
+const asynchandler=(requesthandler)=>{
+
+    (req,res,next)=>{
+        Promise.resolve(requesthandler(req,res,next)).catch((err)=>next(err));
+    }
+
+}
+
+export{asynchandler}
